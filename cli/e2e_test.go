9803d6f9bc3b1106680b965b2148d72cbb541814
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2024-10-12 15:09:02 -0400

    try to github actions without parallelization

--- cli/e2e_test.go
+++ cli/e2e_test.go
@@ -16,7 +16,11 @@ func TestCLI(t *testing.T) {
 func TestCLIWithDiacritics(t *testing.T) {
 	// reset all cli args, since the golang testing framework changes them
 	os.RemoveAll(configDir)
-	os.Args = os.Args[:1]
-	os.Args = append(os.Args, "https://example-files.online-convert.com/document/txt/example.txt", "--speak-diacritics")
+	origArgs := os.Args
+	os.Args = append(origArgs[:1], "https://example-files.online-convert.com/document/txt/example.txt", "--speak-diacritics")
+	RunCLI()
+
+	// make sure that after running you can run the list models command and it will work
+	os.Args = append(origArgs[:1], "https://example-files.online-convert.com/document/txt/example.txt", "--list-models")
 	RunCLI()
 }
